24a7c24c0b946221ee1284b79585b7106d9db8e7
Janna Timmer <jltimmer@users.noreply.github.com>
2023-07-02 12:06:20 -0400

    Add django admin site support for puppies

--- intsureview_be/intsureview_be/apps/api/models.py
+++ intsureview_be/intsureview_be/apps/api/models.py
@@ -7,3 +7,6 @@ class Puppy(models.Model):
     breed = models.CharField(max_length=30)
     color = models.CharField(max_length=30)
     weight = models.IntegerField()
+
+    def __str__(self):
+        return self.name
